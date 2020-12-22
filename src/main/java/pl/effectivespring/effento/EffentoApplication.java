165d02c53d61d6bbda98f687432029fe977b97be
Łukasz Monkiewicz <lmonkiewicz@gmail.com>
2020-12-22 22:05:09 +0100

    Configures JSON logging

--- src/main/java/pl/effectivespring/effento/EffentoApplication.java
+++ src/main/java/pl/effectivespring/effento/EffentoApplication.java
@@ -1,9 +1,15 @@
 package pl.effectivespring.effento;
 
+import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import pl.effectivespring.effento.events.EventConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        EventConfigurationProperties.class
+})
 public class EffentoApplication {
 
     public static void main(String[] args) {
